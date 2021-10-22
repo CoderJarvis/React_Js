0170a9a21fa66767dc62039ccb73fc6c7d1fdced
saswat <saswatkumarpanda515@gmail.com>
2021-10-22 22:08:25 +0530

    build done

--- src awesomeapp/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Heading(props) {
-    return (
-        <div>
-            <h1 style={{textAlign:"center",fontSize:"50px",color:"red"}}>{props.head}</h1>
-        </div>
-    )
-}
