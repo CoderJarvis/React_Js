0460f6261b0d45eee5b212447950fd416ecb927a
saswat <saswatkumarpanda515@gmail.com>
2021-10-27 00:46:35 +0530

    build done

--- project_1_sknews/src/components/CardBootstrap.jsx
+++ project_1_sknews/src/components/CardBootstrap.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 export default function CardBootstrap(props) {
-    return (
-        <>
-         <div className="card container" style={{width: "20rem", height:'30rem'}}>
-  <img  src={props.img}  className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-    <p className="card-text">{props.content}</p>
-    <a href="/" className="btn btn-primary">{props.btn}</a>
-  </div>
-</div>   
-        </>
-    )
+  return (
+    <>
+      <div className="card container" style={{ width: "20rem", height: '30rem' }}>
+        <img src={props.img} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.content}</p>
+          <a href="/" className="btn btn-primary">{props.btn}</a>
+        </div>
+      </div>
+    </>
+  )
 }
