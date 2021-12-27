44aee6249b5dcd8433dd1da65d103e6ce7212aa6
saswat <saswatkumarpanda515@gmail.com>
2021-12-27 23:02:37 +0530

    modified

--- project_1_Sk_E_Commerce/src/components/Home.jsx
+++ project_1_Sk_E_Commerce/src/components/Home.jsx
@@ -3,45 +3,44 @@ import home1 from './img/home1.jfif';
 import home2 from './img/home2.jfif';
 import home3 from './img/home3.jfif';
 
-export default function
-    () {
+export default function Home() {
     return (
         <>
 
 
-<div className=" netflix my-0">
-            <div id="carouselExampleCaptions" className="carousel slide my-0" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <div className=" netflix my-0">
+                <div id="carouselExampleCaptions" className="carousel slide my-0" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src={home3} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={home2} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={home1} className="d-block w-100" alt="..." />
+                            <div className="carousel-caption d-none d-md-block">
+                            </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="visually-hidden">Next</span>
+                    </button>
                 </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={home3} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={home2} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={home1} className="d-block w-100" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                        </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
 
             </div>
         </>
